4c1805f4276a4bf3037347f3d72f9c4df6e31a42
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2025-04-29 10:23:48 +1200

    Adapted css solution to support legacy templates

--- renderer.php
+++ renderer.php
@@ -72,9 +72,15 @@ class gradingform_btec_renderer extends plugin_renderer_base {
                 }
             }
         }
-        $criteriontemplate = html_writer::start_tag('tr', ['class' => 'criterion' . $criterion['class'] . ' criterion_level_' .
-         strtolower($criterion['shortname']) . ' criterion_level_' . strtolower(substr($criterion['shortname'], 0, 1)),
-                    'id' => '{NAME}-criteria-{CRITERION-id}']);
+
+        $criteriontemplatetrclass = 'criterion' . $criterion['class'];
+
+        if ($mode != gradingform_btec_controller::DISPLAY_EDIT_FULL) {
+            $criteriontemplatetrclass .= ' criterion_level_' . strtolower($criterion['shortname']) .
+                ' criterion_level_' . strtolower(substr($criterion['shortname'], 0, 1));
+        }
+        $criteriontemplate = html_writer::start_tag('tr', array('class' => $criteriontemplatetrclass,
+                    'id' => '{NAME}-criteria-{CRITERION-id}'));
         $descriptionclass = 'description';
         if ($mode == gradingform_btec_controller::DISPLAY_EDIT_FULL) {
             $criteriontemplate .= html_writer::start_tag('td', ['class' => 'controls']);
